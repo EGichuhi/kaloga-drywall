14274df69693f7dafa6348d2a3087758e45daa5e
EGichuhi <105747015+EGichuhi@users.noreply.github.com>
2025-06-30 22:33:06 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kaloga-drywall/',
+  base: '/', // ← important fix!
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
