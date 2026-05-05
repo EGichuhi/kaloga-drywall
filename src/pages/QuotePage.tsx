2fa081ad01c588c24b162c22fc635a50d1dfbc62
EGichuhi <105747015+EGichuhi@users.noreply.github.com>
2026-05-04 21:02:50 -0400

    Add chat widget script to QuotePage
    Added chat widget script to QuotePage component.

--- src/pages/QuotePage.tsx
+++ src/pages/QuotePage.tsx
@@ -422,7 +422,7 @@ const QuotePage = () => {
                 </div>
               </form>
             </div>
-            
+            <script src="https://widgets.leadconnectorhq.com/loader.js" data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js" data-widget-id="69f941133c2ab8cb1136a409" data-source="WEB_USER"></script>
             {/* Info Section */}
             <div className="md:w-1/3 bg-teal p-8 text-white">
               <h3 className="text-xl font-semibold mb-6">Contact Information</h3>
